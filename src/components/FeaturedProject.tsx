5609e1698d955b9d206944ddd68096a391a21146
Camilo Sierra <csierra74@unab.edu.co>
2025-02-19 19:54:37 -0500

    Actualizacion: Arreglo de la escala en el Modal de Imagen expandida

--- src/components/FeaturedProject.tsx
+++ src/components/FeaturedProject.tsx
@@ -263,7 +263,7 @@ export default function FeaturedProject() {
           <motion.div
             className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
             initial={{ opacity: 0 }}
-            animate={{ opacity: 1, scale: 2 }}
+            animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             onClick={() => setSelectedImage(null)}
           >
